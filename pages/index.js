a863da93402baca92346cc76f805a73272e12de4
Yurii Hasiuk <hasiuk.george@gmail.com>
2020-11-26 17:23:21 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className='app'>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      App landing
+    </div>
+  )
+}
