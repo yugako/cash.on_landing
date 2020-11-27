d2f9279225857ad6643334191d0ef519f4d76253
Yurii Hasiuk <hasiuk.george@gmail.com>
2020-11-27 13:23:08 +0200

    Commit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      App landing
+      App landing sketch
     </div>
   )
 }
